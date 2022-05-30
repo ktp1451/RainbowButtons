f2008bfec2379ce2fba09ce07313fc59322b2307
Katie Patterson <katiepatterson@Katies-Work-MacBook.local>
2022-05-30 11:16:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
 
-function App() {
+//creating a function to display 7 seperate buttons representing the colors of the rainbow
+export default function RainbowButtons() {
+  
+  const btnArray = [ 
+    //creating an array/list of objects/data types to display the different buttons and their coresponding colors and labels.
+
+    //an object is composed of key value pairs
+    //const exampleObject = {
+	    //left side of the colon is the key : right side the value;
+    //}
+    { color: 'red', label: 'red' },
+    { color: 'orange', label: 'orange' },
+    { color: 'yellow', label: 'yellow' },
+    { color: 'green', label: 'green' },
+    { color: 'blue', label: 'blue' },
+    { color: 'indigo', label: 'indigo' },
+    { color: 'violet', label: 'violet' }
+  ] 
+
+  //
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="RainbowButtons">
+
+        { btnArray.map((button) => (
+          //MUI Button component
+          <Button 
+            //inline CSS to render the color of the button as well as the label text within the button
+            style={{backgroundColor: button.label, color: "white"}}
+            //
+            key={button.label} 
+            //click event to display in the console the name/label of each button once it's clicked
+            onClick={() => console.log('this button is: ', button.label)}
+          >
+            {/* adds the label text to the button */}
+            {button.label}
+          </Button>
+        ))} 
     </div>
   );
 }
 
-export default App;
+
