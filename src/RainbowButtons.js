036059bab79b3efed694fc0f1da90b78794d549c
Katie Patterson <ktpatterson14@mgail.com>
2022-05-30 14:05:59 -0500

    updated code

--- src/RainbowButtons.js
+++ src/RainbowButtons.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
 
-//creating a function to display 7 seperate buttons representing the colors of the rainbow
+//creating a function component to display 7 seperate buttons representing the colors of the rainbow
 export default function RainbowButtons() {
-  
+
+  //declare a variable for an array/list
   const btnArray = [ 
     //creating an array/list of objects/data types to display the different buttons and their coresponding colors and labels.
-
     //an object is composed of key value pairs
     //const exampleObject = {
 	  //left side of the colon is the key : right side the value;
     //}
+    //key uniquely identifying each item being rendered when iterating through array
     { color: 'red', label: 'red' },
     { color: 'orange', label: 'orange' },
     { color: 'yellow', label: 'yellow' },
@@ -21,10 +22,10 @@ export default function RainbowButtons() {
     { color: 'violet', label: 'violet' }
   ] 
 
-  //
+  //return statement that returns what is going to be displayed on the page every time you use the RainbowButton component
   return (
     <div className="RainbowButtons">
-
+        {/* loops through the array btnArray to create the MUI Button component for each item in the array */}
         { btnArray.map((button) => (
           //MUI Button component
           <Button 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import RainbowButtons from './App';
+import RainbowButtons from './RainbowButtons';
 import reportWebVitals from './reportWebVitals';
 
 
