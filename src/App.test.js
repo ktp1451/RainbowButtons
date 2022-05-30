8d9ab0ea82500dffbe96b9ff05f95b1793a2d6a0
Katie Patterson <ktpatterson14@mgail.com>
2022-05-30 14:07:57 -0500

    updated readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import RainbowButton from './RainbowButtons';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<RainbowButton />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/RainbowButtons.js
+++ src/RainbowButtons.js
@@ -2,44 +2,32 @@ import React from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
 
-//creating a function component to display 7 seperate buttons representing the colors of the rainbow
-export default function RainbowButtons() {
+//create a function component to display 7 seperate buttons representing the colors of the rainbow
+export default function RainbowButton() {
 
-  //declare a variable for an array/list
-  const btnArray = [ 
-    //creating an array/list of objects/data types to display the different buttons and their coresponding colors and labels.
-    //an object is composed of key value pairs
-    //const exampleObject = {
-	  //left side of the colon is the key : right side the value;
-    //}
-    //key uniquely identifying each item being rendered when iterating through array
-    { color: 'red', label: 'red' },
-    { color: 'orange', label: 'orange' },
-    { color: 'yellow', label: 'yellow' },
-    { color: 'green', label: 'green' },
-    { color: 'blue', label: 'blue' },
-    { color: 'indigo', label: 'indigo' },
-    { color: 'violet', label: 'violet' }
-  ] 
+  //create an array/list of objects/data types to display the different buttons and their corresponding colors and labels. Since we have the same strings for both the key : value, we can combine them to create 1 string of objects
+  const btnColorArray = [
+    'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'
+  ]
 
   //return statement that returns what is going to be displayed on the page every time you use the RainbowButton component
   return (
-    <div className="RainbowButtons">
-        {/* loops through the array btnArray to create the MUI Button component for each item in the array */}
-        { btnArray.map((button) => (
-          //MUI Button component
-          <Button 
-            //inline CSS to render the color of the button as well as the label text within the button
-            style={{backgroundColor: button.label, color: "white"}}
-            //displays the button label on the button 
-            key={button.label} 
-            //click event to display in the console the name/label of each button once it's clicked
-            onClick={() => console.log('this button is: ', button.label)}
-          >
-            {/* adds the label text to the button */}
-            {button.label}
-          </Button>
-        ))} 
+    <div className="RainbowButton">
+      {/* loop through btnColorArray to create the MUI Button Component for each item in the array and using the item to label/handle the onclick event => */}
+      { btnColorArray.map((button) => (
+        //MUI Button Component
+        <Button
+        // inline CSS to render the color of the button as well as the label text within the button
+        style = {{backgroundColor: button.label, color: 'white'}}
+        //displays the button label on then button 
+        key = {button.label}
+        //click event to display in the console the name/label of each button once it's clicked
+        onClick = {() => console.log('This button is: ', button.label)}
+        >
+        {/* adds the label text to the button */}
+        {button.label}
+        </Button>
+      ))}
     </div>
   );
-}
+};
